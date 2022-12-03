55f4ecf14c072974a95a276397a13c1a0127b502
Kishan Kumar <undefined.kishan@gmail.com>
2022-12-03 21:18:06 +0530

    add initial loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { LinearProgress } from '@mui/material';
+import { useState, useEffect } from 'react';
 export default function App({ Component, pageProps }: AppProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000)
+  }, [])
   return (
-      <Component {...pageProps} />
+    <>
+      {isLoading ? <div><LinearProgress color='error' /></div> : <Component {...pageProps} />}
+
+    </>
   )
 }
